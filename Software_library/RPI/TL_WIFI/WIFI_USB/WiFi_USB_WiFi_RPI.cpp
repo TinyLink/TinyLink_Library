50832f94ea8c004913da89aa09b7045f03308771
Zhihao Cheng <chengzh@emnets.org>
2018-06-04 15:31:12 +0800

    Add nmcli control for wifi

--- Software_library/RPI/TL_WIFI/WIFI_USB/WiFi_USB_WiFi_RPI.cpp
+++ Software_library/RPI/TL_WIFI/WIFI_USB/WiFi_USB_WiFi_RPI.cpp
@@ -14,50 +14,18 @@ namespace tinylink {
 	bool Wifi_Network::init() { return 1;} // dummy funtion	
 
 	bool Wifi_Network::join(const char* SSID, const char* PassW) {
-		// configuration
 		if (!SSID){
 			perror("SSID is null string\n");
 			exit(-1);
 		}
-		system("sudo chmod 111 /home/pi/TinyLink_Config/wifi_config.sh");
-		string comm = string("/home/pi/TinyLink_Config/wifi_config.sh ") + SSID + " " + PassW;
-		int result = system(comm.c_str());
-		if (-1 == result){
-			perror("Error configuring! by fkb");
-			exit(-1); 
-		}
-		else if( 127 == result ) {
-			perror("Error calling /bin/sh! when configured by fkb");
-			exit(-1);
-		}
-		else if ( 512 == result) {
-			perror("Error excuting configuration script! by fkb");
-			exit(-1);
-		}
-		else if( 0 != result) {
-			perror("Error others causes! by fkb");
-			exit(-1);
-		}
-
-		//sleep(3);
-		// check whether already joined the wifi_network
-		cout << "Success configurration\n";
-		char buf[200];
-		FILE* fp = popen("sudo iwconfig", "r");
-		fgets(buf, sizeof(buf), fp);
-		//cout << buf <<endl;
-		pclose(fp);
-		string str(buf, strlen(buf) - 1);
-		int m = str.find("unassociated");
-		if( -1 == m) {
-			cout << "Success join the SSID"<<endl;
+		system("sudo nmcli device connect wlan1");
+		string comm = string("sudo nmcli device wifi connect ")+string(SSID)+" password "+string(PassW)+" ifname wlan1";
+		int res = system(comm.c_str());
+		if(-1 == res){
+			return 0;
+		}else{
 			return 1;
 		}
-		else {
-			cout << "Failture to join the SSID and please check the passW!" << endl;
-			return 0;
-		}	
-
 	}	
 
 	bool Wifi_Network::join(const string& SSID, const string& PassW) {
@@ -70,7 +38,7 @@ namespace tinylink {
 	
 
 	bool Wifi_Network::disjoin(){
-		int result = system("sudo connmanctl disable wifi");
+		int result = system("sudo nmcli device disconnect wlan1");
 		if ( 0 == result) {
 			cout << "Success disjoin network"<<endl;
 			return 1;
