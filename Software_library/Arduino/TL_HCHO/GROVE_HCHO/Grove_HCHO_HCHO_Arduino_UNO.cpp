338cec98671e816656185b4f301da0a72cc86b51
Sebastian Chen <30519526+csp23@users.noreply.github.com>
2018-06-19 14:14:12 +0800

    set analog pin to HCHO_ANALOG_INPUT

--- Software_library/Arduino/TL_HCHO/GROVE_HCHO/Grove_HCHO_HCHO_Arduino_UNO.cpp
+++ Software_library/Arduino/TL_HCHO/GROVE_HCHO/Grove_HCHO_HCHO_Arduino_UNO.cpp
@@ -5,7 +5,7 @@ Grove_HCHO_HCHO_Arduino_UNO::Grove_HCHO_HCHO_Arduino_UNO():Sensor(0.0), R0(36.89
 }
 
 int Grove_HCHO_HCHO_Arduino_UNO::_read(){
-	int sensorValue=analogRead(A0);
+	int sensorValue=analogRead(HCHO_ANALOG_INPUT);
     double Rs=(1023.0/sensorValue)-1;
     double ppm=pow(10.0,((log10(Rs/R0)-0.0827)/(-0.4807)));
     if(ppm>=0){
