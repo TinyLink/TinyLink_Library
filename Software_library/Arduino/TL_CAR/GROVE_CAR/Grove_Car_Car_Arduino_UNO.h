3cb18229a84dfed529a269d42e7f1a891b5e185d
Sebastian Chen <30519526+csp23@users.noreply.github.com>
2018-06-25 17:01:58 +0800

    add TL_CAR for arduino

---
+++ Software_library/Arduino/TL_CAR/GROVE_CAR/Grove_Car_Car_Arduino_UNO.h
@@ -0,0 +1,28 @@
+#ifndef GROVE_CAR_CAR_ARDUINO_UNO_H
+#define GROVE_CAR_CAR_ARDUINO_UNO_H
+
+
+#include "TL_Config.h"
+#include "SparkFunMiniMoto.h"
+
+
+ class Grove_Car_Car_Arduino_UNO
+  {
+          public:
+		  Grove_Car_Car_Arduino_UNO();//set i2c_slave address
+		
+		  void moveForward();
+		  void moveBack();
+		  void stop();
+		  void turnLeft();//right wheel forward, left wheel backward
+		  void turnRight();
+		  
+		  private:
+		  MiniMoto motor0; 
+		  MiniMoto motor1;
+     
+  };
+
+extern Grove_Car_Car_Arduino_UNO TL_Car;
+
+#endif
