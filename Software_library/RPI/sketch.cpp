1905e0a77cc7c485d6fc7e60da86ed98deb24096
csp23 <420748512@qq.com>
2018-07-04 18:45:21 +0800

    add TL_ACCELEROMETER

--- Software_library/RPI/sketch.cpp
+++ Software_library/RPI/sketch.cpp
@@ -79,3 +79,42 @@ void loop() {
 	TL_Time.delayMillis(1000);
 }
 */
+/*
+void loop() {
+        TL_Motor.turnOn();
+        std::cout<<TL_Motor.state()<<std::endl;
+        TL_Time.delayMillis(5000);
+        TL_Motor.turnOff();
+        std::cout<<TL_Motor.state()<<std::endl;
+        TL_Time.delayMillis(2000);
+}
+*/
+/*
+void loop() {
+        TL_Skin_Current.read();
+        double data=TL_Skin_Current.data();
+        data=((1024+2*data)*10000)/(512-data);
+        std::cout<<data<<std::endl;
+        TL_Time.delayMillis(1000);
+}
+*/
+/*
+void loop() {
+        int status=TL_Heart_Rate.read();
+        double data=TL_Heart_Rate.data();
+        std::cout<<data<<"?"<<status<<std::endl;
+        TL_Time.delayMillis(1000);
+}
+*/
+/*
+void loop() {
+          TL_Accelerometer.read();
+          double dataX=TL_Accelerometer.data_x();
+          double dataY=TL_Accelerometer.data_y();
+          double dataZ=TL_Accelerometer.data_z();
+      
+          std::cout<<"X="<<dataX<<"\nY="<<dataY<<"\nZ="<<dataZ<<std::endl;
+          TL_Time.delayMillis(1000);
+}
+*/
+
