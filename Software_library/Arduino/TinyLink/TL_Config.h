f0cc383101fb843b436b20ef05866be96471d122
Zhihao Cheng <chengzh@emnets.org>
2018-12-13 13:33:57 +0800

    Add test file

---
+++ Software_library/Arduino/TinyLink/TL_Config.h
@@ -0,0 +1,9 @@
+#ifndef TL_CONFIG_H
+#define TL_CONFIG_H
+#include "TL_Device_ID.h"
+#define PLATFORM 1
+#define BOARD 1002
+#define TINYLINK_SERIAL ARDUINO_SERIAL
+#define TINYLINK_LIGHT GROVE_LIGHT
+#define LIGHT_ANALOG A0
+#endif
---
+++ Software_library/Arduino/sketch.ino
@@ -0,0 +1,9 @@
+#include "TL_Libraries.h"
+void setup(){
+	TL_Serial.begin(9600);
+}
+
+void loop(){
+	TL_Light.read();
+	TL_Serial.println(TL_Light.data());
+}
