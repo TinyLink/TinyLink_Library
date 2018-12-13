02b762b1cb8a18476183cafb6804665aa142ddf3
Zhihao Cheng <chengzh@emnets.org>
2018-12-13 15:41:43 +0800

    Modify test case for RPI

--- Software_library/RPI/sketch.cpp
+++ Software_library/RPI/sketch.cpp
@@ -1,34 +1,11 @@
 #include "Arduino.h"
-#include <string.h>
 
-char* ch_result;
 
 void setup() {
-
-//	TL_Display.connect();
-//	TL_Display.setRGB(0, 128, 64);
 }
 
 void loop() {
-	
-	TL_Display.show("I am listening");
-
-	ch_result = TL_Voice.recognize(5);
-        printf("%s\n", ch_result);
-        if(strstr(ch_result, "ON")) {
-
-		TL_Display.show("LIGHT ON");
-                TL_Relay.turnOn();
-        }
-        else if (strstr(ch_result, "OFF")) {
-
-		TL_Display.show("LIGHT OFF");
-                TL_Relay.turnOff();
-        }
-	else
-	{
-		TL_Display.show("NOTHING!");
-	}
-	
-	TL_Time.delayMillis(3000);	
+	TL_Light.read();
+	std::cout<<TL_Light.data()<<endl;
+	TL_Time.delayMillis(1000);
 }
